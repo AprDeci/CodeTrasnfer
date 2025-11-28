1371264c7f9dece9b49ea68a57a4a6f39b4b6778
Lushichen <1263868407@qq.com>
2025-11-28 16:40:30 +0800

    Initial commit

---
+++ app/lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:code_transfer/bloc/cubit/counter_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(create: (_) => CounterCubit(), child: CounterPage()),
+    );
+  }
+}
+
+class CounterPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Counter')),
+      body: BlocBuilder<CounterCubit, int>(
+        builder: (context, count) {
+          return Center(
+            child: Text('$count', style: TextStyle(fontSize: 24.0)),
+          );
+        },
+      ),
+      floatingActionButton: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.add),
+              onPressed: () => context.read<CounterCubit>().increment(),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 5.0),
+            child: FloatingActionButton(
+              child: Icon(Icons.remove),
+              onPressed: () => context.read<CounterCubit>().decrement(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
